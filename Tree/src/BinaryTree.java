ef4e49ce87e7807c827ba49289aaafe131461666
Christian Ellis <christian.ellis@cuw.edu>
2015-03-25 19:32:12 -0500

    Binary Tree With Display Functions almost in place

---
+++ Tree/src/BinaryTree.java
@@ -0,0 +1,120 @@
+
+public class BinaryTree 
+{
+	private Node root;
+	private Node temp;
+	
+	public BinaryTree()
+	{
+		this.root = null;
+	}
+	
+	public void displayInOrder()
+	{
+		System.out.println("**** In Order ****");
+		if(this.root == null)
+		{
+			System.out.println("Empty Tree");
+		}
+		else
+		{
+			int count = 0;
+			System.out.println(root.getPayload());
+			
+			if(root.getLeftNode() != null)
+			{
+				temp = root.getLeftNode();
+			}
+			
+			while(temp.getLeftNode() != null)
+			{
+				temp = temp.getLeftNode();
+				count++;
+				if(temp.getLeftNode() == null)
+				{
+					break;
+				}
+			}
+			
+			System.out.println(temp.getPayload());
+			
+			temp = root;
+			
+			while(count != 0)
+			{
+				temp = temp.getLeftNode();
+				System.out.println(temp.getPayload());
+				count--;
+			}
+			
+			if(root.getRightNode() != null)
+			{
+				temp = root.getRightNode();
+				System.out.println(temp.getPayload());
+			}
+			while(temp.getRightNode() != null)
+			{
+				temp = temp.getRightNode();
+				System.out.println(temp.getPayload());
+			}
+		}
+	}
+	
+	public void displayPostOrder()
+	{
+		System.out.println("**** Post Order ****");
+		if(this.root == null)
+		{
+			System.out.println("Empty Tree");
+		}
+		else
+		{
+			int count = 0;
+			temp = this.root.getLeftNode();
+			
+			while(temp.getLeftNode() != null)
+			{
+				temp = temp.getLeftNode();
+				count++;
+				if(temp.getLeftNode() == null)
+				{
+					break;
+				}
+			}
+			System.out.println(temp.getPayload());
+			
+			temp = root;
+			
+			while(count != 0)
+			{
+				if(root.getPayload() <= temp.getPayload())
+				{
+					System.out.println(root.getPayload());
+				}
+				temp = temp.getLeftNode();
+				System.out.println(temp.getPayload());
+				count--;
+			}
+			temp = root;
+			while(temp.getRightNode() != null)
+			{
+				temp = temp.getRightNode();
+				System.out.println(temp.getPayload());
+			}
+		}
+	}
+	
+	public void add(int value)
+	{
+		Node theNode = new Node(value);
+		if(this.root == null)
+		{
+			this.root = theNode;
+		}
+		else
+		{
+			this.root.addNode(theNode);
+		}
+	}
+	
+}
---
+++ Tree/src/Driver.java
@@ -0,0 +1,19 @@
+
+public class Driver 
+{
+
+	public static void main(String[] args) 
+	{
+		BinaryTree bt = new BinaryTree();
+		bt.add(2);
+		bt.add(5);
+		bt.add(7);
+		bt.add(1);
+		bt.add(0);
+		bt.add(3);
+		bt.add(-4);
+		bt.displayInOrder();
+		bt.displayPostOrder();
+	}
+
+}
