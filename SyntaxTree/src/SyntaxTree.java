88e2ac4d910ac7858a9ae00886595393d6437c3e
Christian Ellis <christian.ellis@cuw.edu>
2015-04-29 20:47:44 -0500

    Struggling

---
+++ SyntaxTree/src/SyntaxTree.java
@@ -0,0 +1,17 @@
+
+public class SyntaxTree extends Expression
+{
+	private VarExpression var1;
+	private VarDefStatement statement1;
+	private MathExpression Expression1;
+	
+	public SyntaxTree()
+	{
+		this.var1 = var1;
+		this.statement1 = statement1;
+		this.Expression1 = Expression1;
+	}
+	
+	
+	
+}
---
+++ SyntaxTree/src/VarExpression.java
@@ -0,0 +1,20 @@
+
+public class VarExpression extends Expression
+{
+
+	private String varName;
+
+	public VarExpression(String varName)
+	{
+		this.varName = varName;
+	}
+
+	public String getVarName() {
+		return varName;
+	}
+
+	public void setVarName(String varName) {
+		this.varName = varName;
+	}
+	
+}
