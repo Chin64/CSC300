7fa5be8ca119ff8180d4c1aeaff99ce81fb986d6
Christian Ellis <christian.ellis@cuw.edu>
2015-05-06 22:21:56 -0500

    With HW 18

--- SyntaxTree/src/OpExpression.java
+++ SyntaxTree/src/OpExpression.java
@@ -1,16 +1,39 @@
 
 public class OpExpression extends Expression
 {
-	
-private char theOp;
-	
+	private char theOp;
+
 	public OpExpression(char theOp)
 	{
 		this.theOp = theOp;
 	}
-	
+
 	public String toString()
 	{
-		return "" + theOp;
+		return "" + this.theOp;
+	}
+
+	public int applyOperator(int leftNum, int rightNum)
+	{
+		if(this.theOp == '+')
+		{
+			return leftNum + rightNum;
+		}
+		else if(this.theOp == '-')
+		{
+			return leftNum - rightNum;
+		}
+		else if(this.theOp == '*')
+		{
+			return leftNum * rightNum;
+		}
+		else if(this.theOp == '%')
+		{
+			return leftNum % rightNum;
+		}
+		else
+		{
+			return leftNum / rightNum;
+		}
 	}
 }
--- SyntaxTree/src/VarDefStatement.java
+++ SyntaxTree/src/VarDefStatement.java
@@ -1,9 +1,9 @@
 
 public class VarDefStatement 
 {
-
 	private VarExpression theVarExpr;
 	private MathExpression theMathExpr;
+	private Variable theVariable;
 	
 	public VarDefStatement(VarExpression theVarExpr, MathExpression theMathExpr)
 	{
@@ -11,12 +11,22 @@ public class VarDefStatement
 		this.theMathExpr = theMathExpr;
 	}
 	
+	public VarDefStatement(Variable theVariable)
+	{
+		this.theVariable = theVariable;
+	}
+	
 	public String toString()
 	{
-		//write this such that it rebuilds the String version
-		//of the original statement.  You MAY NOT just use the
-		//store string variable theStmt.
-		
-		return theVarExpr.toString() + "=" + theMathExpr.toString() + ";";
+		//return "hello world " + super.toString();
+		//write this such that it rebuilds the String version of the original statement.  You MAY NOT just use the store string variable theStmt.
+		if(theMathExpr != null)
+		{
+			return "Original Statement: " + this.theVarExpr.toString() + " = " + this.theMathExpr.toString() + ";";
+		}
+		else
+		{
+			return this.theVariable.toString();
+		}
 	}
 }
